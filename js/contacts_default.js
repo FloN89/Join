56f65ba73732e2479dd7e27020ce08b81c4a0870
OsmanAnk <osman.annak@hotmail.de>
2025-11-23 13:15:45 +0100

    Add initial loading functionality and display contact name; enhance success message visibility

--- js/contacts_default.js
+++ js/contacts_default.js
@@ -17,10 +17,38 @@ async function createContact() {
     contactCreated();
 }
 
+// async function checkContactExists(errorMessage, user) {
+//     const existName = await loadData("contacts/" + name);
+//     const existMail = await loadData("contacts/" + mail);
+//     const existPhone = await loadData("contacts/" + phone);
+//     if (existName || existMail || existPhone) {
+//         // errorMessage.classList.add("errorMessage")
+//         // errorMessage.textContent = "Your username is already taken. Please choose another one.";
+//         return;
+//     } else {
+//         // errorMessage.classList.remove("errorMessage")
+//         // errorMessage.textContent = "";
+//         return;
+//     }
+// }
+
 function contactCreated() {
     let successRef = document.getElementById("successfulCreated");
     successRef.classList.add("show");
     setTimeout(() => {
         successRef.classList.remove("show");
     }, 1000);
+}
+
+
+async function getInitals() {
+    const contacts = await loadData("contacts/");
+    const name = Object.keys(contacts)[0];
+    let rgx = new RegExp(/(\p{L}{1})\p{L}+/, 'gu');
+    let initials = [...name.matchAll(rgx)] || [];
+    initials = (
+        (initials.shift()?.[1] || '') + (initials.pop()?.[1] || '')
+    ).toUpperCase();
+
+    console.log(initials);
 }
